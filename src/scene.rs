23930668421149accb216dd7d2a1e7f60b596245
Ralph Caraveo <deckarep@gmail.com>
2017-04-09 21:47:59 -0700

    New backgrounds, getting ready for parallax scrolling sucka.

--- src/scene.rs
+++ src/scene.rs
@@ -7,15 +7,32 @@ use sdl2::render::Texture;
 use sdl2::image::LoadTexture;
 
 pub struct Scene {
-    bg: Texture,
+    layer0: Texture,
+    layer1: Texture,
+    layer2: Texture,
+    layer3: Texture,
+    layer4: Texture,
 }
 
 impl Scene {
     pub fn new(renderer: &mut Renderer) -> Scene {
-        let path = Path::new("res/imgs/background.png");
-        let texture = renderer.load_texture(path).unwrap();
-
-        Scene { bg: texture }
+        Scene {
+            layer0: renderer
+                .load_texture(Path::new("res/imgs/layer_01_1920 x 1080.png"))
+                .unwrap(),
+            layer1: renderer
+                .load_texture(Path::new("res/imgs/layer_02_1920 x 1080.png"))
+                .unwrap(),
+            layer2: renderer
+                .load_texture(Path::new("res/imgs/layer_03_1920 x 1080.png"))
+                .unwrap(),
+            layer3: renderer
+                .load_texture(Path::new("res/imgs/layer_04_1920 x 1080.png"))
+                .unwrap(),
+            layer4: renderer
+                .load_texture(Path::new("res/imgs/layer_05_1920 x 1080.png"))
+                .unwrap(),
+        }
     }
 
     pub fn update(&mut self) {
@@ -23,10 +40,30 @@ impl Scene {
     }
 
     pub fn paint(&self, renderer: &mut Renderer) {
-        let mut current_texture = &self.bg;
+        let mut current_texture = &self.layer0;
         renderer
             .copy(&mut current_texture, None, None)
-            .expect("Background should have rendered.");
+            .expect("Layer0 should have rendered.");
+
+        let mut current_texture = &self.layer1;
+        renderer
+            .copy(&mut current_texture, None, None)
+            .expect("Layer1 should have rendered.");
+
+        let mut current_texture = &self.layer2;
+        renderer
+            .copy(&mut current_texture, None, None)
+            .expect("Layer2 should have rendered.");
+
+        let mut current_texture = &self.layer3;
+        renderer
+            .copy(&mut current_texture, None, None)
+            .expect("Layer3 should have rendered.");
+
+        let mut current_texture = &self.layer4;
+        renderer
+            .copy(&mut current_texture, None, None)
+            .expect("Layer4 should have rendered.");
     }
 
     pub fn restart(&mut self) {}
